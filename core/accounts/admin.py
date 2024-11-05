005fcd19af0fd8b5c3bb8b00d8e065a8db25d290
Mobin <mobin04dev@gmail.com>
2024-11-05 18:21:58 +0330

    feat: Coding Custom User Admin

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['email', 'is_superuser', 'is_active']
+    list_filter =['email', 'is_superuser', 'is_active']
+    searching_fields = ['email']
+    ordering = ['email']
+    
+    fieldsets = (
+        ('Authentication', {
+            'fields': (
+                'email', 'password'
+                )
+            }),
+        ('Permissions', {
+            'fields': (
+                'is_superuser', 'is_staff', 'is_active'
+                )
+            }),
+    )
+    
+    add_fieldsets = (
+        ('Authentication', {
+            'fields': (
+                'email', 'password1', 'password2' 
+                )
+            }),
+        ('Permissions', {
+            'fields': (
+                'is_superuser', 'is_staff', 'is_active'
+                )
+            }),
+    )
