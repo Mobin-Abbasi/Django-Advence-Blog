cc5244655df861a04cda1ef5cf5e3d363f4d28cd
Mobin <mobin04dev@gmail.com>
2024-11-06 21:50:20 +0330

    chore: serving some media, static config

--- core/core/settings.py
+++ core/core/settings.py
@@ -115,6 +115,10 @@ MEDIA_URL = 'media/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+STATICFILES_DIRS =  [
+    BASE_DIR / 'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
