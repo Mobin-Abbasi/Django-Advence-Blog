e317a160c727e3df7f4af4de783e5d8d611c8699
Mobin <mobin04dev@gmail.com>
2024-11-06 22:11:31 +0330

    feat: coding category and post model and writing admin.py in blog app

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post, Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_filter = ['name']
+    
+    
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author','status', 'created_date', 'updated_date', 'published_date']
+    list_filter = ['author','status', 'created_date', 'updated_date', 'published_date']
+    searching_fields = ['title', 'content', 'author', 'status', 'created_date', 'updated_date', 'published_date']
+    ordering = ['title', 'author','status', 'created_date', 'updated_date', 'published_date']
+    
--- core/blog/models.py
+++ core/blog/models.py
@@ -1,33 +1,38 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth import get_user_model
 
 
-# class Category(models.Model):
-#     name = models.CharField(max_length=250)
+# getting user model object
+User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
     
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
 
-# class Post(models.Model):
-#     """
-#     This is a class to define posts in a blog app.
-#     """
-#     author = models.ForeignKey(User, on_delete=models.CASCADE)
-#     image = models.ImageField(null=True, blank=True)
-#     title = models.CharField(max_length=250)
-#     content = models.TextField()
-#     status = models.BooleanField(default=True)
-#     category = models.ForeignKey('Category', on_delete=models.SET_NULL, related_name='subcategories', null=True)
+class Post(models.Model):
+    """
+    This is a class to define posts in a blog app.
+    """
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, related_name='subcategories', null=True)
+    image = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    status = models.BooleanField(default=True)
     
-#     created_date = models.DateTimeField(auto_now_add=True)
-#     updated_date = models.DateTimeField(auto_now=True)
-#     published_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
     
-#     class Meta:
-#         ordering = ['-created_date']
-#         indexes = [
-#             models.Index(fields=['-created_date'])
-#         ]
+    class Meta:
+        ordering = ['-created_date']
+        indexes = [
+            models.Index(fields=['-created_date'])
+        ]
 
-#     def __Str__(self):
-#         return self.title
+    def __Str__(self):
+        return self.title
