dd9660835245fd77b3128dad12f8c6bdf881737d
Mobin <mobin04dev@gmail.com>
2024-11-08 03:04:57 +0330

    training class based view and permissions

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index_view, name='fbv-test'),
+    path('post/', views.PostListView.as_view(), name='posts'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/create/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-edit'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Post, Category
+from .forms import PostForm
 
 
-def index_view(request):
-    return render(request, 'index.html')
+class PostListView(ListView):
+    model = Post
+    template_name = 'index.html'
+    context_object_name = 'posts'
+    
+    
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'detail.html'
+    
+    
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    success_url = '/blog/post/'
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = '/blog/post/'
+    
+    
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/blog/post/'
