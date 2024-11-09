53dee30d58f407ee66fe45fe6808339d89a3b8e6
Mobin <mobin04dev@gmail.com>
2024-11-09 20:55:57 +0330

    feat: create serializer file and coding api views and urls

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,7 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
+from .serializers import PostSerializer
+from ...models import Post
 
 
 @api_view()
 def post_list(request):
-    return Response({"name":"Mobin"})
+    posts = Post.objects.filter(status=True)
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
+
+
+@api_view()
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
