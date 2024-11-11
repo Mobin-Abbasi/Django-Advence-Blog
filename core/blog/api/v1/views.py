257759ff72b689fe958a765e1684022001104a78
Mobin <mobin04dev@gmail.com>
2024-11-11 21:10:21 +0330

    feat: migrate post_list and post_detail to apiview

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,4 +1,3 @@
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import (
     IsAuthenticated,
     IsAdminUser,
@@ -6,11 +5,16 @@ from rest_framework.permissions import (
 )
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+
 from django.shortcuts import get_object_or_404
 
 from .serializers import PostSerializer
 from ...models import Post
 
+
+"""
+from rest_framework.decorators import api_view, permission_classes
 
 @api_view(["GET", "POST"])
 @permission_classes([IsAuthenticated])
@@ -24,8 +28,7 @@ def post_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-
-
+        
 @api_view(["GET", "PUT", "DELETE"])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def post_detail(request, id):
@@ -39,6 +42,57 @@ def post_detail(request, id):
         serializer.save()
         return Response(serializer.data)
     elif request.method == "DELETE":
+        post.delete()
+        return Response(
+            {"message": "Post deleted successfully"}, status=status.HTTP_204_NO_CONTENT
+        )
+
+"""
+
+
+class PostList(APIView):
+    """getting a list of posts and creating new post"""
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+
+    def get(self, request):
+        """retrieving a list of posts"""
+        post = Post.objects.filter(status=True)
+        serializer = PostSerializer(post, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        """creating a post with provided data"""
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class PostDetail(APIView):
+    """retrieving, updating, or deleting a post"""
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+
+    def get(self, request, id):
+        """retrieving a post"""
+        post = get_object_or_404(Post, id=id)
+        serializer = self.serializer_class(post)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        """updating a post with provided data"""
+        post = get_object_or_404(Post, id=id)
+        serializer = self.serializer_class(post, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, id):
+        """deleting a post"""
+        post = get_object_or_404(post, id=id)
         post.delete()
         return Response(
             {"message": "Post deleted successfully"}, status=status.HTTP_204_NO_CONTENT
