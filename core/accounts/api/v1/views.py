2650fb2f742b3b66b082337b39026e41e8d86212
Mobin <mobin04dev@gmail.com>
2024-11-19 00:53:12 +0330

    feat: add api registration feature

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
+
 from .serializers import RegistrationSerializer
 
 
@@ -13,3 +14,4 @@ class RegistrationAPIView(generics.GenericAPIView):
             serializer.save()
             data = {"email": serializer.validated_data["email"]}
             return Response(data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
