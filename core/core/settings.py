0bb7dd139bfa2c86be6df7eaefde4a6cce70214e
Mobin <mobin04dev@gmail.com>
2024-11-19 01:14:00 +0330

    feat: Add Token Authentication in rest framework seetting

--- core/core/settings.py
+++ core/core/settings.py
@@ -148,8 +148,9 @@ AUTH_USER_MODEL = "accounts.User"
 # restframework config
 
 REST_FRAMEWORK = {
-    # "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
-    # "DEFAULT_AUTHENTICATION_CLASSES": [
-    #     "rest_framework_simplejwt.authentication.JWTAuthentication",
-    # ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
 }
