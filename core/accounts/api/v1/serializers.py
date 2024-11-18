5993ddc42619ec65f852f4625bb9a8fd2e0071dc
Mobin <mobin04dev@gmail.com>
2024-11-19 00:14:37 +0330

    feat: coding user serializer and api view with generic view

---
+++ core/accounts/api/v1/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from ...models import User
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    """Serializer for registration"""
+
+    class Meta:
+        model = User
+        fields = ["email", "password"]
--- core/core/urls.py
+++ core/core/urls.py
@@ -23,7 +23,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
     path("blog/", include("blog.urls", namespace="blog")),
     path(
         "swagger/output.json",
