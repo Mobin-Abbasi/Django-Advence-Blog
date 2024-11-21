7c5b7ac5c01e2af06dc4c7d1b483c20107ab3a96
Mobin <mobin04dev@gmail.com>
2024-11-21 19:44:51 +0330

    feat: change accounts models structure

--- core/accounts/models/accounts.py
+++ core/accounts/models/accounts.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin,
 )
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class UserManager(BaseUserManager):
@@ -63,28 +61,3 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    first_name = models.CharField(max_length=250)
-    last_name = models.CharField(max_length=250)
-    description = models.TextField(blank=True)
-    image = models.ImageField(null=True, blank=True)
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ["-created_date"]
-        indexes = [
-            models.Index(fields=["-created_date"]),
-        ]
-
-    def __str__(self):
-        return self.user.email
-
-
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
---
+++ core/accounts/models/profile.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .accounts import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    description = models.TextField(blank=True)
+    image = models.ImageField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_date"]
+        indexes = [
+            models.Index(fields=["-created_date"]),
+        ]
+
+    def __str__(self):
+        return self.user.email
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
