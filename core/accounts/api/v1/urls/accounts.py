66fd4b12313d8fa574a0f50c9646f92e4ec238ed
Mobin <mobin04dev@gmail.com>
2024-11-21 19:49:05 +0330

    feat: change accounts api urls structure

---
+++ core/accounts/api/v1/urls/accounts.py
@@ -0,0 +1,23 @@
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+from django.urls import path
+from .. import views
+
+
+urlpatterns = [
+    path("registration/", views.RegistrationAPIView.as_view(), name="registration"),
+    path("token/login/", views.CustomObtainAuthToken.as_view(), name="token-login"),
+    path("token/logout/", views.CustomDiscardAuthToken.as_view(), name="token-logout"),
+    path("jwt/create/", views.CustomTokenObtainPairView.as_view(), name="token-access"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("jwt/verify/", TokenVerifyView.as_view(), name="token-verify"),
+    path(
+        "change-password/",
+        views.ChangePasswordAPIView.as_view(),
+        name="change-password",
+    ),
+]
---
+++ core/accounts/api/v1/urls/profile.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .. import views
+
+
+urlpatterns = [
+    path("profile/", views.ProfileAPIView.as_view(), name="profile"),
+]
