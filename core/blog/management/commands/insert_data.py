9a78718c9fdeceb09b76ae3e0b45b81f98a3f43c
Mobin <mobin04dev@gmail.com>
2024-11-27 21:54:58 +0330

    add commands file and create fake users and profile in database with faker modul

---
+++ core/blog/management/commands/insert_data.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+
+from accounts.models import User, Profile
+from ...models import Post, Category
+
+
+class Command(BaseCommand):
+    help = "inserting dummy data"
+
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        self.fake = Faker()
+
+    def handle(self, *args, **options):
+        user = User.objects.create_user(email=self.fake.email(), password="Test@123456")
+        profile = Profile.objects.get(user=user)
+        profile.first_name = self.fake.first_name()
+        profile.last_name = self.fake.last_name()
+        profile.description = self.fake.paragraph(nb_sentences=5)
+        profile.save()
